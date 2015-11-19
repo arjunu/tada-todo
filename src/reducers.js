9920473da1d1b75f45290c59b98f188529fd4a3b
Sandeep S Kumar <sanygeek@gmail.com>
2015-11-19 17:11:48 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/reducers.js

--- src/reducers.js
+++ src/reducers.js
@@ -4,24 +4,23 @@ const initialState = [{
     list: [
         {id: 0, name: "Milk", done: false},
         {id: 1, name: "Eggs", done: true},
-        {id: 2, name: "Bean bag", done: true}]
-},
-    {
-        id: 1,
-        title: "Hit list",
-        list: [
-            {id: 0, name: "Vinoj", done: false},
-            {id: 1, name: "Sandeep", done: true},
-            {id: 3, name: "Amala", done: true},
-            {id: 4, name: "Dixy", done: true},
-            {id: 5, name: "Ajay", done: true},
-            {id: 6, name: "Ashwin", done: true},
-            {id: 7, name: "Yashin", done: true},
-            {id: 8, name: "Mudassir", done: true},
-            {id: 9, name: "Ishan", done: true}
-        ]
-    }
-];
+        {id: 2, name: "Bean bag", done: true}
+    ]
+}, {
+    id: 0,
+    title: "Hit list",
+    list: [
+        {id: 0, name: "Vinoj", done: false},
+        {id: 1, name: "Sandeep", done: true},
+        {id: 3, name: "Amala", done: true},
+        {id: 4, name: "Dixy", done: true},
+        {id: 5, name: "Ajay", done: true},
+        {id: 6, name: "Ashwin", done: true},
+        {id: 7, name: "Yashin", done: true},
+        {id: 8, name: "Mudassir", done: true},
+        {id: 9, name: "Ishan", done: true}
+    ]
+}];
 
 
 export function rootReducer(state = initialState, action) {
@@ -29,16 +28,16 @@ export function rootReducer(state = initialState, action) {
     switch (action.type) {
             
         case 'CREATE_TASKGROUP':
-                    return [
-                        ...state, 
-                        {   id: state.reduce((maxId, taskGroup) => Math.max(taskGroup.id, maxId), -1) + 1,
-                            title:'New Task Group',
-                            list: []
-                        }
-                    ];
+            return [
+                ...state,
+                {   id: state.reduce((maxId, taskGroup) => Math.max(taskGroup.id, maxId), -1) + 1,
+                    title:'New Task Group',
+                    list: []
+                }
+            ];
             
         case 'REMOVE_TASKGROUP':
-                return state.filter(taskGroup => taskGroup.id !== action.taskGroupId);
+            return state.filter(taskGroup => taskGroup.id !== action.taskGroupId);
             
         case 'ADD_TITLE':
             
