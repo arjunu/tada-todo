1b767f0b87ee133fafad3ea28716fbccfa6fe164
Sandeep S Kumar <sanygeek@gmail.com>
2015-11-20 12:04:18 +0530

    ....

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ class App extends Component {
             ));
 
         return <div className="to-do-wrapper">  
-            <h1 className="fleft">Tada ToDo</h1>
+            <h1 className="fleft">Tada Todo</h1>
             <SearchTask onSearch={actions.searchTask}/>
             <div className="to-do__task-group-wrapper clearall clearfix">
                 {taskGroupElements}
